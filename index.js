9d0c7324b22a1d6e0bafc8e31c579d49e9bcf540
Tk yoon <y-friend@daum.net>
2021-10-09 21:32:47 +0900

    2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const rootFolder = "C:/Users/tkyoon/Desktop/2021-01_20210402";
+const rootFolder = "C:/Users/윤태경/Desktop/camera/202109";
 const fs = require("fs");
 
 let cnt = 0;
@@ -7,6 +7,7 @@ let currentFolderPath = "";
 const files = fs.readdirSync(rootFolder);
 console.log(files.length);
 
+/** 폴더내 파일 50개씩 폴더 나누기 [TK Yoon(y-friend@daum.net) 2021-10-09 21:32:09] */
 files.forEach((file) => {
   //   if (makeFolderIdx === 0 || (cnt % 50 === 0 && files.length !== cnt)) {
   if (cnt % 50 === 0 && files.length !== cnt) {
