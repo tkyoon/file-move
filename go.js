0e2eb6bf2376c6b402fb2e448516a450e5196bf8
TK Yoon <y-friend@daum.net>
2022-01-09 18:32:02 +0900

    비디오 폴더 나누기

--- go.js
+++ go.js
@@ -59,13 +59,36 @@ const divideByCount50 = async (rootFolder) => {
     cnt++;
     });
 }
+const divideByCount15 = async (rootFolder) => {
+    let cnt = 0;
+    let makeFolderIdx = 0;
+    let currentFolderPath = "";
+    const files = fs.readdirSync(rootFolder);
+    console.log(files.length);
+
+    /** 폴더내 파일 50개씩 폴더 나누기 [TK Yoon(y-friend@daum.net) 2021-10-09 21:32:09] */
+    files.forEach((file) => {
+    //   if (makeFolderIdx === 0 || (cnt % 50 === 0 && files.length !== cnt)) {
+    if (cnt % 15 === 0 && files.length !== cnt) {
+        makeFolderIdx++;
+        currentFolderPath = `${rootFolder}/${makeFolderIdx}`;
+        fs.mkdirSync(currentFolderPath);
+    }
+
+    fs.renameSync(`${rootFolder}/${file}`, `${currentFolderPath}/${file}`);
+    cnt++;
+    });
+}
 
 const go = async() => {
-    const rootFolder = "C:/Users/user/Desktop/p";
+    const rootFolder = "C:/Users/user/Desktop/DCIM/Camera";
     const makeFolders = await divideByDate(rootFolder);
     for(folder of makeFolders) {
         await divideByCount50(folder);
     }
+    
+    //비디오폴더 15개씩 나누기 - 유튜브 기준
+    divideByCount15(`${rootFolder}rootFolder`);
 }
 
 go();
